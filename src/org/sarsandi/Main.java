cf1d63e5f4e19c2b469702d87315c346fc56a05f
RajeevSarsandi <3405678+raj2182@users.noreply.github.com>
2022-10-17 01:00:57 +0530

    initial commit

--- src/org/sarsandi/Main.java
+++ src/org/sarsandi/Main.java
@@ -1,7 +1,7 @@
 package org.sarsandi;
 
 public class Main {
-    public static void main(String arg[]){
+    public static void main(String[] arg){
         System.out.println("Welcome raj!");
 
     }
